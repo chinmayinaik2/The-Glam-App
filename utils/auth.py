eaefa50dc75bfd9843fcb3aca79cf5989d9b0925
chinmayinaik2 <chinmayinaik1212@gmail.com>
2025-05-25 14:24:44 +0530

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -5,15 +5,15 @@ def login():
         "usernames": {
             "admin": {
                 "name": "Admin User",
-                "password": "$2b$12$ynPZQnsX7E9oXhUcCwqjC.4eM/vZ57FYXE3bVwrvcDVPInN9/BrDi"
+                "password": "$2b$12$Q7fZzTFGu1TNTYZIno3KYO9Zg1b6Unk0Ow3tw0OQNPcIwbFhLo9Rm"
             }
         }
     }
 
     authenticator = stauth.Authenticate(
         credentials,
-        "makeup_app",
-        "abcdef",
+        "makeup_app",   # app name
+        "abcdef",       # cookie key
         cookie_expiry_days=1
     )
 
